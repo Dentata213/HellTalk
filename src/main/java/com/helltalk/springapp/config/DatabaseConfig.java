591c517a5810f02e9971f70dac2890f0fa096b82
Dentata213 <102783145+Dentata213@users.noreply.github.com>
2023-01-04 19:35:34 +0900

    서버실행 오류 수정

--- src/main/java/com/helltalk/springapp/config/DatabaseConfig.java
+++ src/main/java/com/helltalk/springapp/config/DatabaseConfig.java
@@ -26,38 +26,7 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class DatabaseConfig {
 
-	
-	//커넥션 풀 미 사용
-	@Bean
-	public DriverManagerDataSource driverManagerDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(driverClassName);
-		dataSource.setUrl(url);
-		dataSource.setUsername(username);
-		dataSource.setPassword(password);
-		return dataSource;
-	}
-	//스프링 커넥션 풀 사용
-	@Value("${jndiName}")
-	private String jndiName;
-	@Bean 
-	JndiObjectFactoryBean jndiObjectFactoryBean() {
-		JndiObjectFactoryBean dataSource = new JndiObjectFactoryBean();
-		/*
-		 *  server.xml이나 context.xml의 
-		    <Context>태그 안의 <ResourceLink global="" 	name="" type="">태그의 
-		     name속성에 지정한 이름
-		 */
-		dataSource.setJndiName(jndiName);
-		/*
-		    아래 세터를 사용하지 않은 경우(디폴트 false) 
-		     위의 jndiName속성의 값으로 해당 WAS서버의 
-			 루트 디렉토리까지 적어줘야 한다 예]java:/comp/env/spring 
-		
-		 */
-		dataSource.setResourceRef(true);
-		return dataSource;
-	}
+
 	//히카리 커넥션 풀 사용:server.xml 및 context.xml에 커넥션 풀 관련 설정 불 필요
 	@Value("${driverClassName}")
 	private String driverClassName;
@@ -68,22 +37,17 @@ public class DatabaseConfig {
 	@Value("${password}")
 	private String password;
 	//히카리 데이타 소스 - 스프링 씨큐리티 적용시에는 주석처리하고 주입받아서 사용
-	/*
+	
 	@Bean 
 	public HikariDataSource hikariDataSource() {
-		System.out.println("driverClassName:"+driverClassName);
-		System.out.println("username:"+username);
-		System.out.println("password:"+password);
+
 		HikariConfig hikariConfig = new HikariConfig();
 		hikariConfig.setDriverClassName(driverClassName);
 		hikariConfig.setJdbcUrl(url);
 		hikariConfig.setUsername(username);
 		hikariConfig.setPassword(password);
 		return new HikariDataSource(hikariConfig);
-	}*/
-	//주입 받기
-	@Autowired
-	private HikariDataSource hikariDataSource;
+	}
 	
 	//https://mybatis.org/spring/ko/factorybean.html
 	//마이바티스 관련 빈
@@ -93,7 +57,7 @@ public class DatabaseConfig {
 		try {
 			SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 			factoryBean.setDataSource(hikariDataSource);//데이타 소스로 히카리 전달
-			factoryBean.setConfigLocation(new ClassPathResource("onememo/mybatis/configuration.xml"));
+			factoryBean.setConfigLocation(new ClassPathResource("mybatis/configuration.xml"));
 	    	factory=factoryBean.getObject();
 	    }
 	    catch(Exception e) {e.printStackTrace();}
