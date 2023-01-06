8ae8684843ad3e89ee0634e538d2c67c10670089
Dentata213 <102783145+Dentata213@users.noreply.github.com>
2023-01-06 18:44:51 +0900

    payment 수정

---
+++ src/main/java/com/helltalk/springapp/controller/payment/PaymentRestApiController.java
@@ -0,0 +1,13 @@
+package com.helltalk.springapp.controller.payment;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.bootpay.Bootpay;
+
+@RestController
+public class PaymentRestApiController {
+	
+	
+	
+}
