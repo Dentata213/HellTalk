15503232495b60fbadf9d61f1373a2e632d54005
jojuhee <minijuhi98@naver.com>
2023-01-10 21:34:14 +0900

    Diet DTO(식단) 생성

--- src/main/java/com/helltalk/springapp/models/DietDTO.java
+++ src/main/java/com/helltalk/springapp/models/DietDTO.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DietDTO {
+	//식단
 	private String diet_no;
 	private String diet_dateCount;
 	private String diet_breakfast;
@@ -19,6 +20,29 @@ public class DietDTO {
 	private String diet_dinner;
 	private String diet_totalKcal;
 	private java.sql.Date diet_date;
+	
+	//한끼
+	private String e_no;
+	private String e_mode;
+	private String e_count;
+	private java.sql.Date e_date;
+	
+	//음식
+	private String food_no;
+	private String food_name;
+	private String food_kcal;
+	private String allergy;
+	private String food_count;
+	private String food_info;
+	
+	//영양성분
+	private String fInfo_no;
+	private String tan;
+	private String dan;
+	private String fat;
+	private String col;
+	private String na;
+	
 	
 
 }
