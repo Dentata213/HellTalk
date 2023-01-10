4e385dd881ccb1a138e97b6b60abb9f2b428a269
jojuhee <minijuhi98@naver.com>
2023-01-10 21:52:10 +0900

    DietDAO 생성

---
+++ src/main/java/com/helltalk/springapp/dao/DietDAO.java
@@ -0,0 +1,34 @@
+package com.helltalk.springapp.dao;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.helltalk.springapp.models.DietDTO;
+
+@Repository
+public class DietDAO {
+		//마이바티스 오라클 주입
+		@Autowired
+		private SqlSessionFactory sqlMapper;
+		@Autowired
+		private SqlSessionTemplate template;
+		
+		public int insert(Map map) {
+			return template.insert("dietInsert",map);
+		}
+		
+		public int update(Map map) {
+			return template.update("dietUpdate",map);
+		}
+		
+		public int delete(Map map) {		
+			return template.delete("dietDelete",map);
+		}
+		
+		
+}
