f07a4216bbebf9895767dfff3555d5c6080f6acb
sua23 <sua4869@naver.com>
2023-01-11 23:53:38 +0900

    MemberServiceImpl 생성

---
+++ src/main/java/com/helltalk/springapp/service/MemberServiceImpl.java
@@ -0,0 +1,54 @@
+package com.helltalk.springapp.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.helltalk.springapp.dao.MemberDAO;
+import com.helltalk.springapp.models.MemberService;
+
+@Service
+public class MemberServiceImpl implements MemberService<MemberDTO>{
+	
+	@Autowired
+	MemberDAO dao;
+
+	@Override
+	public List<MemberDTO> selectUsers(Map map) {
+		List<MemberDTO> users = dao.findAllUser(map);
+		return users;
+	}
+
+	@Override
+	public MemberDTO selectOne(Map map) {
+		return dao.findUserByNo(map);
+	}
+
+	@Override
+	public int insertUser(Map map) {
+		return dao.insert(map);
+	}
+
+	@Override
+	public int updateUser(Map map) {   //회원의 닉네임,키,몸무게 수정가능하게 함
+		return dao.update(map);
+	}
+
+	@Override
+	public int deleteUser(Map map) {
+		// TODO Auto-generated method stub
+		return dao.delete(map);
+	}
+
+	@Override
+	public int idCheck(Map map) {
+		return dao.idChekc(map);
+	}
+	
+	
+	
+	
+
+}
