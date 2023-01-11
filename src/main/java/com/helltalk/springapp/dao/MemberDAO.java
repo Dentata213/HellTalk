90c955e02904a85e060da60bfb2d2d164cbbc5f3
sua23 <sua4869@naver.com>
2023-01-11 23:51:46 +0900

    회원가입 및 아이디체크

--- src/main/java/com/helltalk/springapp/dao/MemberDAO.java
+++ src/main/java/com/helltalk/springapp/dao/MemberDAO.java
@@ -1,5 +1,6 @@
 package com.helltalk.springapp.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -14,19 +15,30 @@ public class MemberDAO {
 	@Autowired
 	private SqlSessionTemplate template;
 	
-	public MemberDTO findAllUser(Map map) {		
-		return template.selectOne("findAllUser", map);
+	public List<MemberDTO> findAllUser(Map map) {		
+		return template.selectList("findAllUser", map);
 	}
 	
 	public MemberDTO findUserByNo(Map map) {		
 		return template.selectOne("findUserByNo", map);
 	}
+	
+	public int idChekc(Map map) {
+		return template.selectOne("idCheck",map);
+	}
+	
+	public int insert(Map map) {		
+		return template.insert("userInsert",map);
+	}
+	
 	public int delete(Map map) {		
 		return template.delete("userDelete",map);
 	}
+	
 	public int update(Map map) {
-		
 		return template.update("userUpdate",map);
 	}
+	
+	
 	
 }
